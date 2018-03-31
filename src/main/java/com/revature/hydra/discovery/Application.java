58e8d36184f9cc549a03d6282f47ef42aee235eb
Bobby M <31968082+sparkzy@users.noreply.github.com>
2018-03-31 18:06:20 -0400

    updated to work with new hydra

--- src/main/java/com/revature/hydra/discovery/Application.java
+++ src/main/java/com/revature/hydra/discovery/Application.java
@@ -6,9 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServiceApplication {
-
+public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServiceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
